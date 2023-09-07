4bead436ee07c9ce8a32b6b7cccde59e4253195b
guillezdev <guillezdev@gmail.com>
2023-09-06 21:18:49 -0400

    errors in styles are fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { getFilms } from "./store/slices/films/thunks";
 import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
-import { setPage, setResetPage, setType } from "./store/slices/films/filmsSlice";
+import {
+  setPage,
+  setResetPage,
+  setType,
+} from "./store/slices/films/filmsSlice";
 
 function App() {
   const [selectFilms, setSelectFilms] = useState("");
-  const page = useSelector((state) => state.films.page)
-  const typeFilm = useSelector((state) => state.films.type)
+  const page = useSelector((state) => state.films.page);
+  const typeFilm = useSelector((state) => state.films.type);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,10 +28,12 @@ function App() {
     <div className="container">
       <header className="header">
         <h1>Search-Muvies</h1>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          dispatch(getFilms(selectFilms, page, typeFilm));
-        }}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(getFilms(selectFilms, page, typeFilm));
+          }}
+        >
           <input
             type="text"
             placeholder="Avengers , The Matrix , Start Wars  . . ."
@@ -35,29 +41,34 @@ function App() {
             required
             value={selectFilms}
           />
-          <button
-            type="submit"
-          >
-            Search
-          </button>
+          <button type="submit">Search</button>
         </form>
         <div>
-          <button onClick={(e) => {
-            e.preventDefault();
-            dispatch(setPage());
-            dispatch(getFilms(selectFilms, page, typeFilm))
-          }} >Next page</button>
-          <button onClick={(e) => {
-            e.preventDefault();
-            dispatch(setResetPage());
-            dispatch(getFilms(selectFilms, 1, typeFilm))
-          }} >reset page</button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(setPage());
+              dispatch(getFilms(selectFilms, page, typeFilm));
+            }}
+          >
+            Next page
+          </button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(setResetPage());
+              dispatch(getFilms(selectFilms, 1, typeFilm));
+            }}
+          >
+            reset page
+          </button>
         </div>
+        <h3>Filters:</h3>
         <div className="filter-container">
-          <button onClick={() => dispatch(setType('movie'))}>Movies</button>
-          <button onClick={() => dispatch(setType('series'))}>Series</button>
-          <button onClick={() => dispatch(setType('episode'))}>Episode</button>
-          <button onClick={() => dispatch(setType(''))}>Reset</button>
+          <button onClick={() => dispatch(setType("movie"))}>Movies</button>
+          <button onClick={() => dispatch(setType("series"))}>Series</button>
+          <button onClick={() => dispatch(setType("episode"))}>Episode</button>
+          <button onClick={() => dispatch(setType(""))}>Reset</button>
         </div>
       </header>
       <hr />
@@ -66,6 +77,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
